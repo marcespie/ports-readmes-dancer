7d695370a7b8d1bc0d66cdb84921829ad71e2720
Anthony J. Bentley <anthony@anjbe.name>
2019-07-18 03:42:50 -0600

    Eliminate inline JavaScript, as there's no way to use it with any CSP.

---
+++ public/javascripts/script.js
@@ -0,0 +1,20 @@
+$('#test_depends_link').click(function() {
+	$('#test-depends-list').slideToggle('fast', function() {
+		// ok
+	});
+	return(false);
+    });
+
+$('#reverse_depends_link').click(function() {
+	$('#reverse-depends-list').slideToggle('fast', function() {
+		// ok
+	});
+	return(false);
+    });
+
+$('#files_list_link').click(function() {
+	$('#files-list').slideToggle('fast', function() {
+		// ok
+	});
+	return(false);
+    });
